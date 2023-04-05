c38a18fb22ed1e667bcf5ac4316ed9e0aab2203d
Oleksii_Baiev <bayevaleksey@gmail.com>
2023-04-05 11:13:39 +0300

    hw15_01

---
+++ HW15/hw15_03_phone.py
@@ -0,0 +1,39 @@
+class Phone:
+    number: str = '123'
+    _counter: int = 0
+
+    def new_number(self):
+        self.number = str(input('Please enter number: '))
+        return self.number
+
+    def call_accept(self):
+        self._counter += 1
+        print('call accepted')
+        return self._counter
+
+    def call(self, __inp):
+        if __inp == 'y':
+            self.call_accept()
+
+    def get_counter(self):
+        return self._counter
+
+def tot_call(telnum):
+    return sum(tel.get_counter() for tel in telnum)
+
+p1 = Phone()
+p2 = Phone()
+p3 = Phone()
+p1.new_number()
+p2.new_number()
+p3.new_number()
+p1.call('n')
+p1.call('y')
+p2.call('y')
+p2.call('n')
+p2.call('y')
+p3.call('n')
+p3.call('y')
+l = [p1, p2, p3]
+print('Numbers: ', p1.number, p2.number, p3.number, 'calls accepted: ', p1._counter + p2._counter + p3._counter)
+print('Total calls: ', tot_call(l))
